0122b1f92e93f478bf19a975055c359a052ae20f
Leha2776 <Lehaparfenov-2776@mail.ru>
2023-02-28 13:31:27 +0500

    first commit

---
+++ Задание 1.js
@@ -0,0 +1,40 @@
+/*
+Задание 1. Классы
+
+class Person{
+    name;
+    age;
+    constructor(personName, personAge){
+        this.name = personName;
+        this.age = personAge;
+    }
+    print(){
+        console.log(`Name: ${this.name}  Age: ${this.age}`);
+    }
+}
+const tom = new Person("Tom", 37);
+tom.print();    // Name: Tom  Age: 37
+const bob = new Person("Bob", 41); 
+bob.print()     // Name: Bob  Age: 41
+
+
+
+Создание класса 
+без описания полей в классе
+
+class Person{
+ 
+    constructor(personName, personAge){
+        this.name = personName;
+        this.age = personAge;
+    }
+    print(){
+        console.log(`Name: ${this.name}  Age: ${this.age}`);
+    }
+}
+const tom = new Person("Tom", 37);
+tom.print();    // Name: Tom  Age: 37
+const bob = new Person("Bob", 41); 
+bob.print()     // Name: Bob  Age: 41
+
+*/
